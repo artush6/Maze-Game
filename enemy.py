be54949b5c6d728efe13f1f86eafcae4f535a67c
balibalouu <basilegamar11@gmail.com>
2026-04-21 09:00:40 +0200

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -24,56 +24,59 @@ class Enemy:
         """
         start = (self.i, self.j)
         
-        came_from = {}
-        g_score = {start: 0}
-        f_score = {start: distance(start, goal)}
+        if goal != start:
+        
+            came_from = {}
+            g_score = {start: 0}
+            f_score = {start: distance(start, goal)}
     
-        open_set = []
-        open_set.append((0 + f_score[start], g_score[start], start))
-        while len(open_set) > 0:
-            f, current_cost, current = min(open_set)
-            open_set.remove((f, current_cost, current))
+            open_set = []
+            open_set.append((0 + f_score[start], g_score[start], start))
+            while len(open_set) > 0:
+                f, current_cost, current = min(open_set)
+                open_set.remove((f, current_cost, current))
 
-            if current == goal:
-                path = []
-                while current in came_from:
-                    path.append(current)
-                    current = came_from[current]
-                self.current_path = path[::-1]
+                if current == goal:
+                    path = []
+                    while current in came_from:
+                        path.append(current)
+                        current = came_from[current]
+                    self.current_path = path[::-1]
 
-            for direction in ['N', 'S', 'W', 'E']:
-                ni, nj = current
-                if direction == 'N':
-                    ni -= 0
-                    nj -= 1
-                elif direction == 'S':
-                    ni += 0
-                    nj += 1
-                elif direction == 'W':
-                    ni -= 1
-                    nj += 0
-                elif direction == 'E':
-                    ni += 1
-                    nj += 0
+                for direction in ['N', 'S', 'W', 'E']:
+                    ni, nj = current
+                    if direction == 'N':
+                        ni -= 0
+                        nj -= 1
+                    elif direction == 'S':
+                        ni += 0
+                        nj += 1
+                    elif direction == 'W':
+                        ni -= 1
+                        nj += 0
+                    elif direction == 'E':
+                        ni += 1
+                        nj += 0
 
-                if (0 <= ni < maze.width and 0 <= nj < maze.height):
-                    next_cell = (ni, nj)
+                    if (0 <= ni < maze.width and 0 <= nj < maze.height):
+                        next_cell = (ni, nj)
                 
 
-                    if (direction == 'N' and not maze.grid[current[0]][current[1]].wall_north) or \
-                        (direction == 'S' and not maze.grid[current[0]][current[1]].wall_south) or \
-                        (direction == 'W' and not maze.grid[current[0]][current[1]].wall_west) or \
-                        (direction == 'E' and not maze.grid[current[0]][current[1]].wall_east):
-                        tentative_g = g_score[current] + 1
-                        if next_cell not in g_score or tentative_g < g_score[next_cell]:
-                            came_from[next_cell] = current
-                            g_score[next_cell] = tentative_g
-                            f_score[next_cell] = tentative_g + distance(next_cell, goal)
-                            open_set.append((f_score[next_cell], tentative_g, next_cell))
+                        if (direction == 'N' and not maze.grid[current[0]][current[1]].wall_north) or \
+                            (direction == 'S' and not maze.grid[current[0]][current[1]].wall_south) or \
+                            (direction == 'W' and not maze.grid[current[0]][current[1]].wall_west) or \
+                            (direction == 'E' and not maze.grid[current[0]][current[1]].wall_east):
+                                tentative_g = g_score[current] + 1
+                                if next_cell not in g_score or tentative_g < g_score[next_cell]:
+                                    came_from[next_cell] = current
+                                    g_score[next_cell] = tentative_g
+                                    f_score[next_cell] = tentative_g + distance(next_cell, goal)
+                                    open_set.append((f_score[next_cell], tentative_g, next_cell))
 
 
         
-        
+    def in_player(self):
+        return self.current_path == []
         
     def draw(self, surface, cell_size):
         x = self.i * cell_size
